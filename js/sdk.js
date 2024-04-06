a628355cead98386f070d75464dbfed12c61d3e3
uzinok <johndoe@example.com>
2024-04-06 13:13:26 +0300

    first commit

---
+++ js/sdk.js
@@ -0,0 +1,13 @@
+(function(g, e, t) {
+  e.GetReview = e.GetReview || {};
+  e.GetReviewTagID = 'Kvm0NI5nO7zq8qXT';
+  e.GetReviewExperimentId = null;
+  var s = g.createElement('script');
+  e[t] = e[t] || function() {
+    (e[t].queue = e[t].queue || []).push(arguments);
+  };
+  s.async = true;
+  s.charset = 'UTF-8';
+  s.src = 'https://app.getreview.io/system/dist/tagmanager.86264291825c9a4a9776.js';
+  if (g.head) { g.head.appendChild(s); }
+})(document, window, 'GetReviewTagManagerLoader');
